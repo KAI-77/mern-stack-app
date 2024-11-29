e772147e575a2cbb5ca0c24c81ef06e933e0026c
KAI-77 <umbongshan2717@gmail.com>
2024-11-29 10:55:15 +0800

    removed footer bg

--- frontend/src/component/footer.jsx
+++ frontend/src/component/footer.jsx
@@ -1,18 +1,9 @@
-import { Text, useColorModeValue } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 export default function Footer() {
-  const textColor = useColorModeValue("gray.200", "#0A0D14");
-
   return (
-    <footer
-      style={{
-        textAlign: "center",
-        padding: "16px",
-        fontSize: "14px",
-        color: "#4A4A4A",
-      }}
-    >
+    <Text textAlign="center" fontSize="sm" py={4}>
       © 2024 Shan Umbong. All rights reserved.
-    </footer>
+    </Text>
   );
 }
