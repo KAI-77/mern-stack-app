a3f71c7658db9846cddb0b2ed71a18219edd0a56
KAI-77 <umbongshan2717@gmail.com>
2024-11-29 10:43:46 +0800

    changed footer color issue

--- frontend/src/component/footer.jsx
+++ frontend/src/component/footer.jsx
@@ -1,8 +1,10 @@
-import { Text } from "@chakra-ui/react";
+import { Text, useColorModeValue } from "@chakra-ui/react";
 
 export default function Footer() {
+  const textColor = useColorModeValue("gray.200", "#0A0D14");
+
   return (
-    <Text textAlign="center" fontSize="sm" color="gray.600" py={4}>
+    <Text textAlign="center" fontSize="sm" color={textColor} py={4}>
       {" "}
       © 2024 Shan Umbong. All rights reserved.
     </Text>
